3a4d796f3e835c5b817ac37a39ebb426daceeded
Hanan <hananumer57@gmail.com>
2026-05-17 16:32:42 +0300

    week 4: frontend html pages added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,POST,PUT,PATCH,DELETE',
-    credentials: true,
+    credentials: false,
   });
   await app.listen(3001);
 }
